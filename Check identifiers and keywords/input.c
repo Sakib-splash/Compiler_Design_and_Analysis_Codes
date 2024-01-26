3111209a5104d5968eb4789c86fe1484e93646de
MD. SAKIBUL ISLAM <mdsakibulislam736@gmail.com>
2024-01-26 15:47:39 +0600

    counting identifiers and keywordsd

---
+++ Check identifiers and keywords/input.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+//this is a comment
+int main()
+{
+    int number = 10;
+    double value = 1.67;
+    printf("The number is: %d\n", number);
+    return =;
+}/*this is a multi line comment
+*/
